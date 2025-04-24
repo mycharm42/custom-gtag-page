d4d05a2294e4515883359613f1a69794f2b06923
mycharm42 <whckami42@gmail.com>
2025-04-24 21:18:29 +0900

    fifth

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ searchBtn.addEventListener("click", handleSearch);
 
 // Product View
 const viewItemBtn = document.getElementById("viewItem");
-const addToCart = document.getElementById("addToCart");
-const purchase = document.getElementById("purchase");
+const addToCartBtn = document.getElementById("addToCart");
+const purchaseBtn = document.getElementById("purchase");
 
 const viewItem = () => {
   gtag("event", "view_item", {
@@ -41,3 +41,47 @@ const viewItem = () => {
 };
 
 viewItemBtn.addEventListener("click", viewItem);
+
+const addToCart = () => {
+  gtag("event", "add_to_cart", {
+    currency: "KRW",
+    value: 129000,
+    items: [
+      // item: [] 은 반드시 포함되어야 전자상거래 태그, 나머지는 커스터마이징 가능
+      {
+        item_id: "EP1001",
+        item_name: "무선 이어폰",
+        currency: "KRW",
+        price: 129000,
+        quantity: 1,
+      },
+    ],
+  });
+  alert("add_to_cart 이벤트 발생");
+};
+
+addToCartBtn.addEventListener("click", addToCart);
+
+const purchase = () => {
+  gtag("event", "purchase", {
+    transaction_id: "T12345",
+    affiliation: "카페24몰",
+    currency: "KRW",
+    value: 129000,
+    tzx: 12900,
+    shipping: 3000,
+    items: [
+      // item: [] 은 반드시 포함되어야 전자상거래 태그, 나머지는 커스터마이징 가능
+      {
+        item_id: "EP1001",
+        item_name: "무선 이어폰",
+        currency: "KRW",
+        price: 129000,
+        quantity: 1,
+      },
+    ],
+  });
+  alert("purchase 이벤트 발생");
+};
+
+purchaseBtn.addEventListener("click", purchase);
